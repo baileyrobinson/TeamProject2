6efe344c427fe749ebc8c45b95f97c93be2f5e61
baileyrobinson <37555330+baileyrobinson@users.noreply.github.com>
2018-05-03 03:32:25 -0400

    blah

---
+++ Assets/Game/Classes/Player/AnimationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public AnimationClip attack;
+    public AnimationClip walk;
+    public AnimationClip idle;
+    Animation anim;
+    bool Attack = false;
+    bool Walk = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+        {
+            anim.clip = walk;
+            anim.Play();
+            Walk = true;
+        }
+        else
+        {
+            anim.clip = idle;
+            anim.Play();
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            anim.clip = attack;
+            anim.Play();
+            Attack = true;
+        }
+    }
+}
